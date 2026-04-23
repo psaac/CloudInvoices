173ef9c35e8eb66a5d411327884c1285f0841147
Pascal Bellebouche <pascal.bellebouche@kleegroup.com>
2026-04-23 12:22:55 +0200

    Updated env script for Windows

--- env.ps1
+++ env.ps1
@@ -1,5 +1,5 @@
 # Loads and exports environment variables from .env file
-envFilePath = Join-Path -Path $PSScriptRoot -ChildPath ".env"
+$envFilePath = Join-Path -Path $PSScriptRoot -ChildPath ".env"
 if (Test-Path -Path $envFilePath) {
     Get-Content -Path $envFilePath | ForEach-Object {
         if ($_ -match "^\s*([^#][^=]+?)\s*=\s*(.+)\s*$") {
